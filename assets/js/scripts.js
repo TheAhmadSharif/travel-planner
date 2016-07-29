6e68d7ece38185d7ecf9360ad74aeae591daedca
Ahmad <ahmadshorif@gmail.com>
2016-07-29 10:36:26 +0600

    responsive | animation | select dropdown icon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,37 @@
-	jQuery(document).ready(function($) {
+jQuery(document).ready(function($) {
 	$('#toggleMenu').click(function(){
-			$('#sidebar').toggleClass('visible-sidebar');
-			$('#mainBlock').toggleClass('move-right');
+			$('#sidebar').css('left', '0');
+			$('#closeTrigger').show(0);
 	});
+
+	$('#closeTrigger').click(function(event) {
+		$('#sidebar').css('left', '-180px');
+		$('.container-margin').css('margin-left', '0');
+		$(this).hide('0');
+		console.log('Hi CloseBar Clicked');
+	});
+
+
+	/* Closebar */
+	
+	function sideabrVisibility () {
+				var w = $( window ).width();
+				if ( w < 769) {
+					 	$('#toggleMenu').fadeIn('100');
+						$('#sidebar').css('left', '-300px');
+						$('.container-margin').css('margin-left', '0');
+				}
+
+				if (w > 769) {
+						$('#sidebar').css('left', '0');
+						$('.container-margin').css('margin-left', '180px');
+						$('#toggleMenu').hide('0');
+				}
+				console.log('Hello Resize');
+		}
+
+		sideabrVisibility();
+		window.addEventListener('resize', sideabrVisibility);
+
 
 });
